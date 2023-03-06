d68407907cc9d275777f194dde398e6e3a580129
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-03-06 00:39:55 +0000

    Eng comp

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,59 +1,59 @@
 const Engineer = require("../lib/Engineer");
 
 test("creates an engineer object", () => {
-  const engineer = new Engineer("Jack", "123", "jack@gmail.com", "jackhub");
-  expect(engineer.name).toBe("Jack");
+  const engineer = new Engineer("Sand", "123", "sand@webmail.com", "sandhub");
+  expect(engineer.name).toBe("Sand");
   expect(engineer.id).toBe("123");
-  expect(engineer.email).toBe("jack@gmail.com");
-  expect(engineer.github).toBe("jackhub");
+  expect(engineer.email).toBe("sandk@webmail.com");
+  expect(engineer.github).toBe("sandhub");
 });
 
 test("gets engineer's name", () => {
   const result = new Engineer(
-    "Jack",
+    "Sand",
     "123",
-    "jack@gmail.com",
-    "jackhub"
+    "sand@webmail.com",
+    "sandhub"
   ).getName();
-  expect(result).toEqual("Jack");
+  expect(result).toEqual("sand");
 });
 
 test("gets engineer's ID", () => {
   const result = new Engineer(
-    "Jack",
+    "Sand",
     "123",
-    "jack@gmail.com",
-    "jackhub"
+    "sand@webmail.com",
+    "sandhub"
   ).getId();
   expect(result).toEqual("123");
 });
 
 test("gets engineer's email", () => {
   const result = new Engineer(
-    "Jack",
+    "Sand",
     "123",
-    "jack@gmail.com",
-    "jackhub"
+    "sand@webmail.com",
+    "sandhub"
   ).getEmail();
-  expect(result).toEqual("jack@gmail.com");
+  expect(result).toEqual("sand@webmail.com");
 });
 
 test("gets engineer's github", () => {
   const result = new Engineer(
-    "Jack",
+    "Sand",
     "123",
-    "jack@gmail.com",
-    "jackhub"
+    "sand@webmail.com",
+    "sandhub"
   ).getGithub();
-  expect(result).toEqual("jackhub");
+  expect(result).toEqual("sandhub");
 });
 
 test("gets engineer's role", () => {
   const result = new Engineer(
-    "Jack",
+    "Sand",
     "123",
-    "jack@gmail.com",
-    "jackhub"
+    "sand@webmail.com",
+    "sandhub"
   ).getRole();
   expect(result).toEqual("Engineer");
 });
